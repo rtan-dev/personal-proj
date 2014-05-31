2c7a1c4543167ccc461bc630f140851b82c39aff
Ralph Tan <r.tan@klab.com>
2014-05-31 22:29:53 +0800

    commit changes

--- app/controllers/item_controller.php
+++ app/controllers/item_controller.php
@@ -12,12 +12,17 @@ class ItemController extends AppController
     public function index()
     {
         $character = $this->start();
-        $tab = Param::get('tab', 'potions');
+        $tab = Param::get('tab', 'useable');
 
         switch ($tab) {
             case Item::ITEM_TYPE_USEABLE:
+                $items = $character->getItemStorage()->getPotions();
                 break;
             case Item::ITEM_TYPE_LOOT:
+                $items = $character->getItemStorage()->getLoots();
+                break;
+            default:
+                $this->render('error/invalid_item_type');
         }
 
         $this->set(get_defined_vars());
---
+++ app/views/error/invalid_item_type.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ralph
+ * Date: 5/31/14
+ * Time: 10:25 PM
+ */
+?>
+<div class="login">
+    <div class="alert-container1">
+        <h1 class="form-h1">Error.</h1>
+        <p class="p-alert">
+           Invalid item type.
+            <br />
+            <a href="<?php eh(url('item/index')) ?>">Click here to go back.</a>
+        </p>
+    </div>
+</div>
